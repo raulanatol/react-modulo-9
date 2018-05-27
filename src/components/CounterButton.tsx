0686d990dace3083750f6d357c64c760755a82a3
raulanatol <raul@natol.es>
2018-05-27 19:59:20 +0100

    C2

--- src/components/CounterButton.tsx
+++ src/components/CounterButton.tsx
@@ -1,7 +1,26 @@
 import * as React from "react";
 
-export default class CounterButton extends React.Component<{}, {}> {
+export interface CounterButtonState {
+  numberOfClicks: number;
+}
+
+export default class CounterButton extends React.Component<{}, CounterButtonState> {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      numberOfClicks: 0
+    };
+
+    this.incrementCount = this.incrementCount.bind(this);
+  }
+
+  incrementCount() {
+    this.setState({ numberOfClicks: this.state.numberOfClicks + 1 });
+  }
+
   render() {
-    return <button>0</button>;
+    return <button onClick={this.incrementCount}>{this.state.numberOfClicks}</button>;
   }
 }
--- test/components/CounterButton.test.tsx
+++ test/components/CounterButton.test.tsx
@@ -8,5 +8,11 @@ describe('CounterButton', () => {
       const wrapper = shallow(<CounterButton/>);
       expect(wrapper.find('button').text()).toBe('0');
     });
+
+    test('Debería renderizar un 1 cuando se haya hecho un click en el botón', () => {
+      const wrapper = shallow(<CounterButton/>);
+      wrapper.find('button').simulate('click');
+      expect(wrapper.find('button').text()).toBe('1');
+    });
   });
 });
