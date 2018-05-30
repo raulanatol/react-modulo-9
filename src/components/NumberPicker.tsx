db04aa2c78f7ee75ff3737c3b667899dd99d3d63
raulanatol <raul@natol.es>
2018-05-30 13:52:23 +0100

    Style react way

--- src/components/NumberPicker.tsx
+++ src/components/NumberPicker.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { CSSProperties } from 'react';
 
 export interface NumberPickerProps {
   value?: number;
@@ -9,6 +10,47 @@ export interface NumberPickerProps {
 export interface NumberPickerState {
   value?: number;
 }
+
+const numberPickerStyle: CSSProperties = {
+  width: '150px',
+  height: '32px',
+  display: 'flex',
+  alignItems: 'center',
+  position: 'relative',
+  border: '1px solid #EDEEF4',
+  boxShadow: '0 2px 1px 0 rgba(0, 0, 0, 0.03)',
+  color: '#385871',
+  borderRadius: '5px'
+};
+
+const numberPickerLabel: CSSProperties = {
+  width: '100px',
+  height: '32px',
+  textAlign: 'center',
+  padding: 0,
+  outline: 'none',
+  fontFamily: 'OpenSans-Bold, sans-serif',
+  color: '#385871',
+  borderRadius: '5px',
+  lineHeight: '30px'
+};
+
+const numberPickerButtons: CSSProperties = {
+  backgroundColor: '#4CAF50',
+  border: 'none',
+  color: 'white',
+  width: '30px',
+  height: '30px',
+  textAlign: 'center',
+  textDecoration: 'none',
+  display: 'inline-block',
+  borderRadius: '50%',
+  fontSize: '25px',
+  paddingBottom: '5px',
+  lineHeight: '25px',
+  cursor: 'pointer'
+};
+
 
 export default class NumberPicker extends React.Component<NumberPickerProps, NumberPickerState> {
 
@@ -40,10 +82,10 @@ export default class NumberPicker extends React.Component<NumberPickerProps, Num
 
 
   render() {
-    return <div className="number-picker">
-      <button className="number-picker-buttons" onClick={this.increase}>+</button>
-      <p className="number-picker-label">{this.state.value}</p>
-      <button className="number-picker-buttons" onClick={this.decrease}>-</button>
+    return <div style={numberPickerStyle}>
+      <button style={numberPickerButtons} onClick={this.increase}>+</button>
+      <p style={numberPickerLabel}>{this.state.value}</p>
+      <button style={numberPickerButtons} onClick={this.decrease}>-</button>
     </div>;
   }
 }
