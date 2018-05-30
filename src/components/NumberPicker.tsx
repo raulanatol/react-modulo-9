a63d78e620e39a107ca8d2eb548fef1cb56e8e0e
raulanatol <raul@natol.es>
2018-05-30 16:36:46 +0100

    CSS-MODULES

--- src/components/NumberPicker.tsx
+++ src/components/NumberPicker.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { CSSProperties } from 'react';
+import * as styles from './NumberPicker.css';
 
 export interface NumberPickerProps {
   value?: number;
@@ -10,47 +10,6 @@ export interface NumberPickerProps {
 export interface NumberPickerState {
   value?: number;
 }
-
-const numberPickerStyle: CSSProperties = {
-  width: '150px',
-  height: '32px',
-  display: 'flex',
-  alignItems: 'center',
-  position: 'relative',
-  border: '1px solid #EDEEF4',
-  boxShadow: '0 2px 1px 0 rgba(0, 0, 0, 0.03)',
-  color: '#385871',
-  borderRadius: '5px'
-};
-
-const numberPickerLabel: CSSProperties = {
-  width: '100px',
-  height: '32px',
-  textAlign: 'center',
-  padding: 0,
-  outline: 'none',
-  fontFamily: 'OpenSans-Bold, sans-serif',
-  color: '#385871',
-  borderRadius: '5px',
-  lineHeight: '30px'
-};
-
-const numberPickerButtons: CSSProperties = {
-  backgroundColor: '#4CAF50',
-  border: 'none',
-  color: 'white',
-  width: '30px',
-  height: '30px',
-  textAlign: 'center',
-  textDecoration: 'none',
-  display: 'inline-block',
-  borderRadius: '50%',
-  fontSize: '25px',
-  paddingBottom: '5px',
-  lineHeight: '25px',
-  cursor: 'pointer'
-};
-
 
 export default class NumberPicker extends React.Component<NumberPickerProps, NumberPickerState> {
 
@@ -82,10 +41,10 @@ export default class NumberPicker extends React.Component<NumberPickerProps, Num
 
 
   render() {
-    return <div style={numberPickerStyle}>
-      <button style={numberPickerButtons} onClick={this.increase}>+</button>
-      <p style={numberPickerLabel}>{this.state.value}</p>
-      <button style={numberPickerButtons} onClick={this.decrease}>-</button>
+    return <div className={styles.container}>
+      <button className={styles.buttons} onClick={this.increase}>+</button>
+      <p className={styles.label}>{this.state.value}</p>
+      <button className={styles.buttons} onClick={this.decrease}>-</button>
     </div>;
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
-    entry: './src/index.tsx',
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },
-    module: {
-        rules: [
-            { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ }
-        ]
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    }
+  mode: 'production',
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
+      {
+        test: /\.css?$/, use: ['style-loader', { loader: 'css-loader', options: { modules: true, importLoaders: 1, localIdentName: '[sha1:hash:hex:4]' } }]
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  }
 };
