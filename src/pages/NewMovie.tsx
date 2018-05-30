4f5bc15a6682d21cee515d181964dc1f3f6b263b
raulanatol <raul@natol.es>
2018-05-30 18:30:54 +0100

    Uncontrolled-form input

--- src/pages/NewMovie.tsx
+++ src/pages/NewMovie.tsx
@@ -1,32 +1,16 @@
 import * as React from 'react';
 
-export interface NewMovieState {
-  name: string;
-  description: string;
-  genre: string;
-}
+export default class NewMovie extends React.Component {
+  input: HTMLInputElement;
 
-export default class NewMovie extends React.Component<{}, NewMovieState> {
   constructor(props) {
     super(props);
-
-    this.state = {
-      name: '',
-      description: '',
-      genre: ''
-    };
-
-    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    alert(`Creando nueva película: ${JSON.stringify(this.state)}`);
+    alert(`Creando nueva película: ${this.input.value}`);
   }
 
   render() {
@@ -34,19 +18,7 @@ export default class NewMovie extends React.Component<{}, NewMovieState> {
       <form onSubmit={this.handleSubmit}>
         <label>
           Name:
-          <input name="name" type="text" value={this.state.name} onChange={this.handleChange}/>
-        </label>
-        <label>
-          Description:
-          <textarea name="description" value={this.state.description} onChange={this.handleChange}/>
-        </label>
-        <label>
-          Genre:
-          <select name="genre" value={this.state.genre} onChange={this.handleChange}>
-            <option value="action">Action</option>
-            <option value="drama">Drama</option>
-            <option value="adventure">Adventure</option>
-          </select>
+          <input ref={c => this.input = c} type="text"/>
         </label>
         <input type="submit" value="Submit"/>
       </form>
